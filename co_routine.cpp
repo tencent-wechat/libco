98042959defcfe8f771b26f3cf481e9dea43ef95
mariohuang <huangduokai@gmail.com>
2016-09-09 22:57:23 +0800

    Update co_routine.cpp

--- co_routine.cpp
+++ co_routine.cpp
@@ -58,46 +58,56 @@ void co_log_err( const char *fmt,... )
 {
 }
 
+#if defined( __LIBCO_RDTSCP__) 
+static unsigned long long counter(void)
+{
+    register uint32_t lo, hi;
+    register unsigned long long o;
+    __asm__ __volatile__ (
+        "rdtscp" : "=a"(lo), "=d"(hi)
+    );
+    o = hi;
+    o <<= 32;
+    return (o | lo);
+}
 
-static unsigned long long counter(void);
 static unsigned long long getCpuKhz()
 {
-	FILE *fp = fopen("/proc/cpuinfo","r");
-	if(!fp) return 1;
-	char buf[4096] = {0};
-	fread(buf,1,sizeof(buf),fp);
-	fclose(fp);
+    FILE *fp = fopen("/proc/cpuinfo","r");
+    if(!fp) return 1;
+    char buf[4096] = {0};
+    fread(buf,1,sizeof(buf),fp);
+    fclose(fp);
 
-	char *lp = strstr(buf,"cpu MHz");
-	if(!lp) return 1;
-	lp += strlen("cpu MHz");
-	while(*lp == ' ' || *lp == '\t' || *lp == ':')
-	{
-		++lp;
-	}
-
-	double mhz = atof(lp);
-	unsigned long long u = (unsigned long long)(mhz * 1000);
-	return u;
+    char *lp = strstr(buf,"cpu MHz");
+    if(!lp) return 1;
+    lp += strlen("cpu MHz");
+    while(*lp == ' ' || *lp == '\t' || *lp == ':')
+    {
+        ++lp;
+    }
+	
+    double mhz = atof(lp);
+    unsigned long long u = (unsigned long long)(mhz * 1000);
+    return u;
 }
+#endif
 
 static unsigned long long GetTickMS()
 {
-	static uint32_t khz = getCpuKhz();
-	return counter() / khz;
+#if defined( __LIBCO_RDTSCP__) 
+    static uint32_t khz = getCpuKhz();
+    return counter() / khz;
+#else
+    struct timeval now = { 0 };
+    gettimeofday( &now,NULL );
+    unsigned long long u = now.tv_sec;
+    u *= 1000;
+    u += now.tv_usec / 1000;
+    return u;
+#endif
 }
 
-static unsigned long long counter(void)
-{
-	register uint32_t lo, hi;
-	register unsigned long long o;
-    __asm__ __volatile__ (
-        "rdtscp" : "=a"(lo), "=d"(hi)
-        );
-	o = hi;
-	o <<= 32;
-	return (o | lo);
-}
 static pid_t GetPid()
 {
     static __thread pid_t pid = 0;
